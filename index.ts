f4bab3831312f5ee84b6be66f770da914325921b
Erich Json Fosse <erich@halloverden.no>
2019-02-24 22:04:57 +0100

    ADD: Barrel file

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from '@utilities/luhn-utilities';
+export * from '@utilities/mod11-utilities';
+export * from '@utilities/ssn-utilities';
+export * from '@validators/account-number-validator';
+export * from '@validators/kid-validator';
+export * from '@validators/organization-number-validator';
+export * from '@validators/ssn-validator';
