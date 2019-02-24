f3aba55b8896517f3fed402468d75b990a14ccd4
Erich Json Fosse <erich@halloverden.no>
2019-02-24 13:03:16 +0100

    ADD: Mod11 utility class + tests

---
+++ src/utilities/mod11.ts
@@ -0,0 +1,56 @@
+export class Mod11 {
+  public static getControlDigit( input: string ): number|null {
+    let weightNumber = 2;
+    let modSum = 0;
+
+    for ( let i = input.length - 1; i >= 0; i-- ) {
+      let digit = parseInt( input.charAt( i ), 10 );
+      if (isNaN(digit)) {
+        return null;
+      }
+      modSum += weightNumber * digit;
+
+      if ( ++weightNumber > 7 ) {
+        weightNumber = 2;
+      }
+    }
+
+    const remainder = ( modSum % 11 );
+
+    switch (remainder) {
+      case 0:
+        return remainder;
+      case 1:
+        return null;
+      default:
+        return 11 - remainder;
+    }
+  }
+
+  public static getControlDigits( input: string, numberOfDigits: number ): (number|null)[] {
+    const controlDigits = [];
+    for (let i = numberOfDigits; i > 0; i--) {
+      let c = Mod11.getControlDigit(input);
+      controlDigits.push(c);
+      input += null === c ? '-' : c.toString();
+    }
+
+    return controlDigits;
+  }
+
+  public static checkControlDigit( input: string ): boolean {
+    return parseInt(input.charAt(input.length - 1), 10) === Mod11.getControlDigit(input.substr(0, input.length - 1));
+  }
+
+  public static checkControlDigits( input: string, numberOfDigits: number ): boolean {
+    for (let i = numberOfDigits; i > 0; i--) {
+      if (!Mod11.checkControlDigit(input)) {
+        return false;
+      }
+
+      input = input.substr(0, input.length - 1);
+    }
+
+    return true;
+  }
+}
---
+++ test/utilities/mod11.spec.ts
@@ -0,0 +1,86 @@
+import { Mod11 } from '@utilities/mod11';
+
+describe('mod11', function() {
+
+  it('getControlDigit() should create correct control digit', function () {
+    let values = [
+      {input: '5102874550', expectedOutput: 1},
+      {input: '51028745501', expectedOutput: null},
+      {input: '0102874553', expectedOutput: 9},
+      {input: '0102874550', expectedOutput: 4},
+      {input: '123456789', expectedOutput: 2},
+      {input: '98765', expectedOutput: 4},
+      {input: '1', expectedOutput: 9}
+    ];
+
+    values.forEach((v) => {
+      let output = Mod11.getControlDigit(v.input);
+      expect(output).toEqual(v.expectedOutput);
+    });
+
+  });
+
+  it('getControlDigits() should return an array of correct control digits', function () {
+    let values = [
+      {input: '5102874550', numberOfDigits: 3, expectedOutput: [1, null, null]},
+      {input: '0102874553', numberOfDigits: 4, expectedOutput: [9, 4, 5, 4]},
+      {input: '0102874550', numberOfDigits: 2, expectedOutput: [4, 1]},
+      {input: '123456789', numberOfDigits: 1, expectedOutput: [2]},
+      {input: '123456789', numberOfDigits: 2, expectedOutput: [2, null]},
+      {input: '98765', numberOfDigits: 5, expectedOutput: [4, 5, null, null, null]},
+      {input: '1', numberOfDigits: 2, expectedOutput: [9, 1]},
+      {input: '1', numberOfDigits: 3, expectedOutput: [9, 1, 0]}
+    ];
+
+    values.forEach((v) => {
+      let output = Mod11.getControlDigits(v.input, v.numberOfDigits);
+      expect(output).toEqual(v.expectedOutput);
+    });
+
+  });
+
+  it('checkControlDigit() should validate control digit', function () {
+    let values = [
+      {input: '51028745501', expectedOutput: true},
+      {input: '51028745502', expectedOutput: false},
+      {input: '01028745539', expectedOutput: true},
+      {input: '01028745534', expectedOutput: false},
+      {input: '01028745504', expectedOutput: true},
+      {input: '01028745505', expectedOutput: false},
+      {input: '1234567892', expectedOutput: true},
+      {input: '1234567895', expectedOutput: false},
+      {input: '987654', expectedOutput: true},
+      {input: '987653', expectedOutput: false},
+      {input: '9876545', expectedOutput: true},
+      {input: '98765454', expectedOutput: false}
+    ];
+
+    values.forEach((v) => {
+      let output = Mod11.checkControlDigit(v.input);
+      expect(output).toEqual(v.expectedOutput);
+    });
+
+  });
+
+  it('checkControlDigits() should validate control digits', function () {
+    let values = [
+      {input: '01028745539454', numberOfDigits: 4, expectedOutput: true},
+      {input: '51028745502', numberOfDigits: 3, expectedOutput: false},
+      {input: '010287455041', numberOfDigits: 2, expectedOutput: true},
+      {input: '01028745534', numberOfDigits: 3, expectedOutput: false},
+      {input: '9876545', numberOfDigits: 2, expectedOutput: true},
+      {input: '19', numberOfDigits: 1, expectedOutput: true},
+      {input: '191', numberOfDigits: 2, expectedOutput: true},
+      {input: '1910', numberOfDigits: 3, expectedOutput: true},
+      {input: '1912', numberOfDigits: 3, expectedOutput: false},
+      {input: '19102', numberOfDigits: 4, expectedOutput: false}
+    ];
+
+    values.forEach((v) => {
+      let output = Mod11.checkControlDigits(v.input, v.numberOfDigits);
+      expect(output).toEqual(v.expectedOutput);
+    });
+
+  });
+
+});
