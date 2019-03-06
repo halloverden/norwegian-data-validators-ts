59ed85839552035653429bafcea4c205d8b529d6
Erich Json Fosse <erich@halloverden.no>
2019-03-06 22:27:10 +0100

    EDIT: Moved index.ts to the src folder

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { LuhnUtilities } from '@utilities/luhn-utilities';
+export { Mod11Utilities } from '@utilities/mod11-utilities';
+export { SsnUtilities } from '@utilities/ssn-utilities';
+export { AccountNumberValidator } from '@validators/account-number-validator';
+export { KidValidator } from '@validators/kid-validator';
+export { OrganizationNumberValidator } from '@validators/organization-number-validator';
+export { SsnValidator } from '@validators/ssn-validator';
