33ef22b77947e302d5d0572afcf63ebb930d4733
Erich Json Fosse <erich@halloverden.no>
2019-03-07 17:37:14 +0100

    ADD: Test coverage now works

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,4 +1,12 @@
 const common = require('./webpack.config');
 const merge = require('webpack-merge');
+const path = require('path');
 
-module.exports = merge(common, {});
+module.exports = merge(common, {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: './'
+  },
+  mode: 'development'
+});
