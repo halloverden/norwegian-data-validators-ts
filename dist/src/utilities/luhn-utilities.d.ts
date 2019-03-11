b2ef73c7a08b55c01408235b4d9c2ab3bcc544e3
Erich Json Fosse <erich@halloverden.no>
2019-03-11 12:59:17 +0100

    EDIT: Ignoring the dist folder

--- dist/src/utilities/luhn-utilities.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getLuhnControlDigit(input: string): number;
-export declare function getLuhnControlDigits(input: string, numberOfDigits: number): number[];
-export declare function checkLuhnControlDigit(input: string): boolean;
-export declare function checkLuhnControlDigits(input: string, numberOfDigits: number): boolean;
--- dist/src/validators/account-number-validator.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function validateAccountNumber(accountNumber: string): boolean;
