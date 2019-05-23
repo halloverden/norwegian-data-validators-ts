cf9c6fcc1f89785a8c6878c68959059ceb3d7d4f
Christopher <christopher@halloverden.no>
2019-05-23 14:04:02 +0200

    FIX: Control digit was not removed before calling getMod11ControlDigit().
    Instead use the checkMod11ControlDigit() method which removes control digit internally.

--- src/validators/account-number-validator.ts
+++ src/validators/account-number-validator.ts
@@ -1,4 +1,4 @@
-import { getMod11ControlDigit } from '../utilities/mod11-utilities';
+import { checkMod11ControlDigit } from '../utilities/mod11-utilities';
 
 export function validateAccountNumber( accountNumber: string ) {
   const validAccountNumberPattern = new RegExp( /\d{11}|\d{4}\.\d{2}\.\d{5}/ );
@@ -8,5 +8,5 @@ export function validateAccountNumber( accountNumber: string ) {
 
   accountNumber = accountNumber.toString().replace( /\./g, '' );
 
-  return parseInt( accountNumber.charAt( accountNumber.length - 1 ), 10 ) === getMod11ControlDigit( accountNumber );
+  return checkMod11ControlDigit( accountNumber );
 }
