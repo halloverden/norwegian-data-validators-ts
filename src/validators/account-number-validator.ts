2f53e0575a7c25c7a74169e26a257ec10e0a2172
Christopher <christopher@halloverden.no>
2019-05-23 14:52:57 +0200

    Cleanup: Avoid assigning to intermediate variable, use regex directly

--- src/validators/account-number-validator.ts
+++ src/validators/account-number-validator.ts
@@ -1,8 +1,7 @@
 import { checkMod11ControlDigit } from '../utilities/mod11-utilities';
 
 export function validateAccountNumber( accountNumber: string ) {
-  const validAccountNumberPattern = new RegExp( /\d{11}|\d{4}\.\d{2}\.\d{5}/ );
-  if ( !validAccountNumberPattern.test( accountNumber ) ) {
+  if ( !/\d{11}|\d{4}\.\d{2}\.\d{5}/.test( accountNumber ) ) {
     return false;
   }
 
